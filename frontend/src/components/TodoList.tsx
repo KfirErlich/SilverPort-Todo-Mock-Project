578eae76a2b35540bc0799867c0ae91e2514e84e
Kfir Erlich <kfirerlich@MacBookPro-2.lan>
2025-11-26 15:34:14 +0200

    adding useContext in order to set global methods and not pass them in the tree

--- frontend/src/components/TodoList.tsx
+++ frontend/src/components/TodoList.tsx
@@ -2,12 +2,12 @@ import type { TodoListProps } from "../interfaces/Todo";
 import TodoTask from "./TodoTask";
 
 
-function TodoList({ todos, handleTaskCompletion, handleDeleteTodo }: TodoListProps) {
+function TodoList({ todos }: TodoListProps) {
     const emptyTodoTask = <div className="text-3xl font-bold p-4">No todos found, please add a new todo!</div>;
   return (
     <div className="flex flex-col gap-2 items-center text-3xl font-bold p-4">
       {todos.length > 0 ? todos.map((todo) => (
-        <TodoTask key={todo.id} todo={todo} handleTaskCompletion={handleTaskCompletion} handleDeleteTodo={handleDeleteTodo} />
+        <TodoTask key={todo.id} todo={todo} />
       )) : emptyTodoTask}
     </div>
   );
